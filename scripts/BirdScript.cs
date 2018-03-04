2b9ba9bf50ad0df6564d4c071e70585dc6b0916f
Ibrar Yunus - (Lenovo Y700) <iy3@st-andrews.ac.uk>
2018-03-04 04:46:01 +0000

    first commit
    Has obstacle generation error - please fix.

---
+++ scripts/BirdScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    // movement speed
+    public float speed = 3;
+
+    // flap force
+    public float force = 300;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // restart the game
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ scripts/Generate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour {
+
+    public GameObject obstacles;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateObstacle()
+    {
+        Instantiate(obstacles);
+    }
+}
